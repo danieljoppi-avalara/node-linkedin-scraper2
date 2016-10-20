454ddbb99d80df57659ce5edbd37233f7991f076
olegdemkovych <oleg.demkovych@temy.co>
2016-10-20 10:54:17 +0300

    Fix tests

--- test/scraper-test.js
+++ test/scraper-test.js
@@ -10,18 +10,18 @@ describe('<LinkedIn Scraper>', function() {
     function validateProfile(profile) {
         should(profile).be.ok();
         should(profile).have.property('name', 'Daniel Henrique Joppi');
-        should(profile).have.property('headline', 'Software Architect na Rospo GeoTech');
-        should(profile).have.property('location', 'Florianópolis, Santa Catarina, Brazil');
+        should(profile).have.property('headline', 'Development Manager at Avalara');
+        should(profile).have.property('location', 'Florianópolis Area, Brazil');
         should(profile).have.property('summary').be.ok();
         should(profile).have.property('industry').be.equal('Program Development');
         should(profile).have.property('languages').length(5);
-        should(profile).have.property('skills').length(45);
-        should(profile).have.property('currentPositions').length(3);
-        should(profile).have.property('pastPositions').length(3);
+        should(profile).have.property('skills').length(49);
+        should(profile).have.property('currentPositions').length(0);
+        should(profile).have.property('pastPositions').length(0);
         should(profile).have.property('educations').length(1);
-        should(profile).have.property('positions').length(8);
+        should(profile).have.property('positions').length(10);
         //should(profile).have.property('honors').length(0);
-        should(profile).have.property('projects').length(5);
+        should(profile).have.property('projects').length(0);
         should(profile).have.property('publicProfileUrl', url);
     }
 
